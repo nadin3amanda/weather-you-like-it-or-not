990bc53fa92b122e3dcea2082790c4fcd960102c
Nadine Amanda <nadine@soar.earth>
2021-11-07 07:05:12 +0800

    added location and weather box on app.js and modified css styling in index.css

--- src/App.js
+++ src/App.js
@@ -5,6 +5,38 @@ const api = {
   base: "https://api.openweathermap.org/data/2.5/",
 };
 function App() {
+  const dateBuilder = (d) => {
+    let months = [
+      "January",
+      "February",
+      "March",
+      "April",
+      "May",
+      "June",
+      "July",
+      "August",
+      "September",
+      "October",
+      "November",
+      "December",
+    ];
+    let days = [
+      "Sunday",
+      "Monday",
+      "Tuesday",
+      "Wednesday",
+      "Thursday",
+      "Friday",
+      "Saturday",
+    ];
+
+    let day = days[d.getDay()];
+    let date = d.getDate();
+    let month = months[d.getMonth()];
+    let year = d.getFullYear();
+
+    return `${day} ${date} ${month} ${year}`;
+  };
   return (
     <div className="App">
       <main>
@@ -15,7 +47,14 @@ function App() {
             placeHolder="Search here..."
           />
         </div>
-        <div className="location-box"></div>
+        <div className="location-box">
+          <div className="location">Quezon City, PH</div>
+          <div className="date">{dateBuilder(new Date())} </div>
+        </div>
+        <div className="weather-box">
+          <div className="temp">25 C</div>
+          <div className="weather">Sunny</div>
+        </div>
       </main>
     </div>
   );
