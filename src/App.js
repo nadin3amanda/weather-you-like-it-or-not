45722d9dc6aa74e9fefae33953c54cba759efb53
Nadine Amanda <nadine@soar.earth>
2021-11-06 20:59:56 +0800

    removed App.css import and header and its valules. Added api key and base.

--- src/App.js
+++ src/App.js
@@ -1,25 +1,11 @@
 import React from "react";
-import "./App.css";
 
+const api = {
+  key: "dc5f0c9a6a3a0f330074417fdf61f7e9";
+  base:'https://api.openweathermap.org/data/2.5';
+}
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+  return <div className="App"></div>;
 }
 
 export default App;
