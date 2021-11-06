957ed4c4a5f8461ed62f7e9b47cee76682342e49
Nadine Amanda <nadine@soar.earth>
2021-11-06 22:16:08 +0800

    added search box

--- src/App.js
+++ src/App.js
@@ -1,11 +1,23 @@
 import React from "react";
 
 const api = {
-  key: "dc5f0c9a6a3a0f330074417fdf61f7e9";
-  base:'https://api.openweathermap.org/data/2.5';
-}
+  key: "dc5f0c9a6a3a0f330074417fdf61f7e9",
+  base: "https://api.openweathermap.org/data/2.5/",
+};
 function App() {
-  return <div className="App"></div>;
+  return (
+    <div className="App">
+      <main>
+        <div className="search-box">
+          <input
+            type="text"
+            className="search-bar"
+            placeHolder="Search here..."
+          />
+        </div>
+      </main>
+    </div>
+  );
 }
 
 export default App;
