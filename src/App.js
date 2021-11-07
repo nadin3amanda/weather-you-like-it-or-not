6f797edf7048a93e97a6da9309abc0b294990b71
Nadine Amanda <nadine@soar.earth>
2021-11-07 10:18:26 +0800

    added state for inputs: location and weather

--- src/App.js
+++ src/App.js
@@ -12,7 +12,11 @@ function App() {
     if (evt.key === "Enter") {
       fetch(`${api.base}weather?q=${query}&units=metric&APPID=${api.key}`)
         .then((res) => res.json())
-        .then((result) => setWeather(result));
+        .then((result) => {
+          setWeather(result);
+          setQuery("");
+          console.log(result);
+        });
     }
   };
   const dateBuilder = (d) => {
@@ -55,16 +59,27 @@ function App() {
             type="text"
             className="search-bar"
             placeHolder="Search here..."
+            onChange={(e) => setQuery(e.target.value)}
+            value={query}
+            onKeyPress={search}
           />
         </div>
-        <div className="location-box">
-          <div className="location">Quezon City, PH</div>
-          <div className="date">{dateBuilder(new Date())} </div>
-        </div>
-        <div className="weather-box">
-          <div className="temp">25 °C</div>
-          <div className="weather">Sunny</div>
-        </div>
+        {typeof weather.main != "undefined" ? (
+          <div>
+            <div className="location-box">
+              <div className="location">
+                {weather.name}, {weather.sys.country}{" "}
+              </div>
+              <div className="date">{dateBuilder(new Date())} </div>
+            </div>
+            <div className="weather-box">
+              <div className="temp">{Math(weather.main.temp)} °C</div>
+              <div className="weather">Sunny</div>
+            </div>
+          </div>
+        ) : (
+          ""
+        )}
       </main>
     </div>
   );
